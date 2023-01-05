9e8af4d79071b368afefdfcde6b53197b8391d6b
Kinue72 <kinue137@gmail.com>
2023-01-06 06:12:37 +0700

    Initial commit

---
+++ book.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"github.com/PuerkitoBio/goquery"
+	"github.com/bmaupin/go-epub"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+type Book struct {
+	*epub.Epub
+
+	Id      string
+	BaseUrl string
+}
+
+var reNote = regexp.MustCompile("\\[note\\d*\\]")
+
+func (b *Book) CrawlChapterBody(url string, retries int) string {
+	if *retriesFlag > 0 && retries > *retriesFlag {
+		return ""
+	}
+
+	resp, err := GetRequest(url, "")
+	if err != nil {
+		return b.CrawlChapterBody(url, retries+1)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		log.Printf("status code: %d\n", resp.StatusCode)
+		return ""
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return b.CrawlChapterBody(url, retries+1)
+	}
+
+	var sb strings.Builder
+
+	title := doc.Find(".title-top > h4").Text()
+	subTitle := strings.Split(doc.Find(".title-top > h6").Text(), " - ")[0] + " - Ngày đăng: " + doc.Find(".title-top > h6 > time").AttrOr("datetime", "Unknown")
+
+	sb.WriteString(`<h2 class="chapter-header">`)
+	sb.WriteString(title)
+	sb.WriteString("</h2>")
+
+	sb.WriteString(`<h4 class="chapter-header">`)
+	sb.WriteString(subTitle)
+	sb.WriteString("</h4>")
+
+	sb.WriteString(`<hr class="header"/>`)
+
+	sb.WriteString(`<div class="reading-content">`)
+
+	doc.Find("#chapter-content > p").Each(func(_ int, selection *goquery.Selection) {
+		if _, ok := selection.Attr("id"); !ok {
+			return
+		}
+
+		//map img
+		selection.Find("img").Each(func(_ int, selection *goquery.Selection) {
+			src := selection.AttrOr("src", "")
+			if src == "" {
+				return
+			}
+			internal := b.CrawlImage(src, true, 0)
+			if internal == "" {
+				return
+			}
+			selection.SetAttr("class", "insert")
+			selection.SetAttr("src", internal)
+		})
+
+		html, _ := selection.Html()
+
+		notes := reNote.FindAllStringSubmatch(html, -1)
+		html = reNote.ReplaceAllString(html, "")
+
+		sb.WriteString("<p>")
+		sb.WriteString(html)
+		sb.WriteString("</p>")
+
+		// poor man note :(
+		if *noteFlag {
+			for _, matches := range notes {
+				note := strings.TrimLeft(matches[0], "[")
+				note = strings.TrimRight(note, "]")
+
+				text, _ := doc.Find("#" + note + " > .note-content_real").Html()
+				if text == "" {
+					continue
+				}
+				sb.WriteString("<p>(")
+				sb.WriteString(note)
+				sb.WriteString(": ")
+				sb.WriteString(text)
+				sb.WriteString(")</p>")
+			}
+		}
+	})
+	sb.WriteString("</div>")
+
+	return sb.String()
+}
+
+func (b *Book) CrawlImage(path string, referer bool, retries int) string {
+	if *retriesFlag > 0 && retries > *retriesFlag {
+		return ""
+	}
+
+	imgPath := filepath.Join("./tmp", b.Id, HashString(path)+filepath.Ext(path))
+	if _, err := os.Stat(imgPath); err == nil {
+		ret, err := b.AddImage(imgPath, "")
+		if err != nil {
+			return b.CrawlImage(path, referer, retries+1)
+		}
+		return ret
+	}
+
+	ref := ""
+	if referer {
+		ref = b.BaseUrl + "/"
+	}
+
+	resp, err := GetRequest(path, ref)
+	if err != nil {
+		return b.CrawlImage(path, referer, retries+1)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		log.Printf("status code: %d\n", resp.StatusCode)
+		return ""
+	}
+
+	out, err := os.Create(imgPath)
+	defer out.Close()
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return b.CrawlImage(path, referer, retries+1)
+	}
+
+	ret, err := b.AddImage(imgPath, "")
+	if err != nil {
+		return b.CrawlImage(path, referer, retries+1)
+	}
+	return ret
+}
---
+++ go.mod
@@ -0,0 +1,18 @@
+module docln-archiver
+
+go 1.19
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/alexsergivan/transliterator v1.0.0
+	github.com/bmaupin/go-epub v1.0.1
+	golang.org/x/text v0.3.6
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/gabriel-vasile/mimetype v1.3.1 // indirect
+	github.com/gofrs/uuid v3.1.0+incompatible // indirect
+	github.com/vincent-petithory/dataurl v0.0.0-20191104211930-d1553a71de50 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+)
